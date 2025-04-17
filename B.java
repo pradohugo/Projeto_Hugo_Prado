df170d581ec932052a53405171c27a281bc6041f
Hugo <lpradohugo1@gmail.com>
2025-04-17 16:14:20 -0300

    Adiciona classes A e B com atributos, getters, setters e métodos MA1/MA2, MB1/MB2

---
+++ B.java
@@ -0,0 +1,31 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    // Getters
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    // Setters
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    // Métodos
+    public void MB1() {
+        System.out.println("Método MB1");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2");
+    }
+}
