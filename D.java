caadc66bb0042b2198b4c993f30930264ac997c1
Hugo <lpradohugo1@gmail.com>
2025-04-17 17:36:50 -0300

    Cria classe D com atributos D1, D2 e métodos MD1 e MD2

---
+++ D.java
@@ -0,0 +1,31 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    // Getters
+    public String getD1() {
+        return D1;
+    }
+
+    public int getD2() {
+        return D2;
+    }
+
+    // Setters
+    public void setD1(String D1) {
+        this.D1 = D1;
+    }
+
+    public void setD2(int D2) {
+        this.D2 = D2;
+    }
+
+    // Métodos
+    public void MD1() {
+        System.out.println("Método MD1");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2");
+    }
+}
